0d4c80e5bc6f479d0bdd8c78c37e4cee1b5033c8
Björn Ahl <bnal@ba78.me>
2019-11-27 20:29:15 +0100

    Adding section 6

---
+++ section6-vue-cli/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,22 @@
+<template>
+    <dive>
+        <p>Server Status: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </dive>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            status: 'Critical'
+        }
+    },
+    methods: {
+        changeStatus() {
+            this.status = 'Normal';
+        }
+    }
+}
+</script>
